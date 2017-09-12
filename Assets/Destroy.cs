93539a3a54c2b1c91053dce2458ad098b0418847
Ryan Hudson <demontoastdt@gmail.com>
2017-09-12 10:55:26 -0400

    trying to simulate a merge conflict please ignore jake

--- Assets/Destroy.cs
+++ Assets/Destroy.cs
@@ -1,10 +1,69 @@
 ﻿using UnityEngine;
+/*
+ * trying to simulate a merge conflict, if you get an email about this jake please ignore
+ *
+ *
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 using System.Collections;
-
+/*
+ * trying to simulate a merge conflict, if you get an email about this jake please ignore
+ *
+ *
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 public class Destroy : MonoBehaviour {
+    /*
+ * trying to simulate a merge conflict, if you get an email about this jake please ignore
+ *
+ *
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+    void destroyObject()
+    /*
+* trying to simulate a merge conflict, if you get an email about this jake please ignore
+*
+*
+* 
+* 
+* 
+* 
+* 
+*/
+    {
+        Destroy (this.gameObject);
+        /*
+ * trying to simulate a merge conflict, if you get an email about this jake please ignore
+ *
+ *
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+    }
 
-	void destroyObject()
-	{
-		Destroy (this.gameObject);
-	}
+    /*
+     * trying to simulate a merge conflict, if you get an email about this jake please ignore
+     *
+     *
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
 }
